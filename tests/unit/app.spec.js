d465b810c54be7046407176177b7b612ec37e0d1
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2020-09-27 16:37:38 -0400

    Fix tests (bad copy'n pasta)

--- tests/unit/app.spec.js
+++ tests/unit/app.spec.js
@@ -1,15 +1,62 @@
-import { createLocalVue, shallow } from "@vue/test-utils";
+// import { createLocalVue, shallowMount } from "@vue/test-utils";
+// import Vuex from "vuex";
+// import App from "@/App";
+//
+// const localVue = createLocalVue();
+// localVue.use(Vuex);
+//
+// describe("App.vue", () => {
+//   let actions;
+//   let store;
+//   let router;
+//
+//   beforeEach(() => {
+//     actions = {
+//       loadDb: jest.fn()
+//     };
+//     store = new Vuex.Store({
+//       state: {},
+//       actions
+//     });
+//     router = {};
+//   });
+//
+//   it("should dispatch action when created", () => {
+//     shallowMount(App, {
+//       localVue,
+//       store,
+//       router,
+//     });
+//     expect(actions.loadDb).toHaveBeenCalled();
+//   });
+// });
+
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import Vue from "vue";
 import Vuex from "vuex";
+import VueRouter from "vue-router";
+import Vuetify from "vuetify";
 import App from "@/App";
 
-const localVue = createLocalVue();
-localVue.use(Vuex);
+describe("MediaDetailComponent.vue", () => {
+  const localVue = createLocalVue();
+  localVue.use(VueRouter);
+  const router = new VueRouter();
 
-describe("App.vue", () => {
   let actions;
   let store;
 
+  const shallowMountFunction = options => shallowMount(App, {
+    Vuetify,
+    localVue,
+    store,
+    router,
+    ...options
+  });
+
   beforeEach(() => {
+    Vue.use(Vuex);
+    Vue.use(Vuetify);
     actions = {
       loadDb: jest.fn()
     };
@@ -19,11 +66,9 @@ describe("App.vue", () => {
     });
   });
 
-  it("should dispatch action when created", () => {
-    shallow(App, {
-      localVue,
-      store
-    });
+  test("render", () => {
+    const wrapper = shallowMountFunction({});
+    expect(wrapper.vm).toBeTruthy();
     expect(actions.loadDb).toHaveBeenCalled();
   });
 });
