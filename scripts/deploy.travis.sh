c4b01d7c1af706e2987d0a3077ed2c9e4d31a1eb
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2020-12-01 17:08:06 -0500

    tweak travis and build

--- scripts/deploy.travis.sh
+++ scripts/deploy.travis.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 if [ "$TRAVIS_OS_NAME" == osx ]; then
     # deploy on mac
-    yarn release
+    yarn electron:build
 else
     # deploy on windows and linux
-    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn release --linux AppImage --win"
+    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn electron:build --linux AppImage --win"
 fi
