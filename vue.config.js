59ba14da0a580b388df86fb37d6d49062d8faef6
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2020-12-02 15:19:55 -0500

    More testing

--- vue.config.js
+++ vue.config.js
@@ -5,30 +5,7 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       preload: "src/preload.js",
-      externals: ["pdffiller"],
-      appId: "{APP_ID}",
-      publish: "github",
-      dmg: {
-        contents: [
-          {
-            x: 110,
-            y: 150
-          },
-          {
-            x: 240,
-            y: 150,
-            type: "link",
-            path: "/Applications"
-          }
-        ]
-      },
-      appImage: {
-        license: "LICENSE"
-      },
-      nsis: {
-        createDesktopShortcut: "always",
-        license: "LICENSE"
-      }
+      externals: ["pdffiller"]
     }
   },
 };
