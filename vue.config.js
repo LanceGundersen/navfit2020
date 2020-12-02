f6feb5ba9f45b7e3a51dedaf13d462f955165464
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2020-12-01 19:46:50 -0500

    Travis build Tweaks

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,29 @@ module.exports = {
     electronBuilder: {
       preload: "src/preload.js",
       externals: ["pdffiller"],
+      appId: "{APP_ID}",
+      publish: "github",
+      dmg: {
+        contents: [
+          {
+            x: 110,
+            y: 150
+          },
+          {
+            x: 240,
+            y: 150,
+            type: "link",
+            path: "/Applications"
+          }
+        ]
+      },
+      appImage: {
+        license: "LICENSE"
+      },
+      nsis: {
+        createDesktopShortcut: "always",
+        license: "LICENSE"
+      }
     }
   },
 };
