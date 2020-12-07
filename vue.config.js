7e870c6a87aa51f9be2ac6beddf3939086c4b7bf
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2020-12-07 16:46:33 -0500

    set applicationId

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,7 @@ module.exports = {
           target: "appx",
         },
         appx: {
+          applicationId: "LanceGundersen.NavFit",
           identityName: "23182LanceGundersen.NavFit",
           publisher: "CN=A2D9DB40-6AB5-4D32-AA2E-C095DDE633A0",
           publisherDisplayName: "Lance Gundersen",
