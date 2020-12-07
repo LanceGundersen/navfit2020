e907560902711642fea9a5d7d92c93e5242b68b3
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2020-12-07 16:13:48 -0500

    Fix windows target data

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,17 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       preload: "src/preload.js",
-      externals: ["pdffiller"]
+      externals: ["pdffiller"],
+      builderOptions: {
+        win: {
+          target: "appx",
+          publisher: "A2D9DB40-6AB5-4D32-AA2E-C095DDE633A0",
+          publisherDisplayName: "Lance Gundersen",
+          identityName: "23182LanceGundersen.NavFit",
+          displayName: "NavFit",
+
+        }
+      }
     }
   },
 };
