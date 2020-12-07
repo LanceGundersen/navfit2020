395ed7b90b2be852cbd78a5322849d7f21bf3fa0
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2020-12-07 16:23:05 -0500

    adjust naming and appx settings

--- vue.config.js
+++ vue.config.js
@@ -9,11 +9,11 @@ module.exports = {
       builderOptions: {
         win: {
           target: "appx",
+        },
+        appx: {
           publisher: "A2D9DB40-6AB5-4D32-AA2E-C095DDE633A0",
           publisherDisplayName: "Lance Gundersen",
-          identityName: "23182LanceGundersen.NavFit",
           displayName: "NavFit",
-
         }
       }
     }
